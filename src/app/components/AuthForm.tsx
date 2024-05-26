14dbc5a95569f9c692a408ce2400ec4ffd8ffb2c
yukimatsu <drying3@gmail.com>
2024-05-26 23:22:13 +0900

    ✨ AuthFormこんぽーねんと作成

--- src/app/components/AuthForm.tsx
+++ src/app/components/AuthForm.tsx
@@ -1,8 +1,40 @@
-// ToDo: 5/26 このUIとロジック作成
+"use client";
+
+import { useState } from "react";
+
+// ToDo: 5/26 フォームUI作成とusername,email,passwordを登録するようにする。画像はデフォルトを設定し、後からアップロードさせる（モーダルで表示もありだと思う）
 export default function AuthForm() {
+  const [activeTab, setActiveTab] = useState("signup");
+  console.log(activeTab);
   return (
-    <div>
-      <p>AuthForm</p>
+    <div className="flex flex-col">
+      <div>
+        <ul className="flex flex-wrap text-sm font-medium text-center text-gray-500 border-b border-gray-200">
+          <li className="me-2">
+            <button
+              onClick={() => setActiveTab("signup")}
+              className={`inline-block px-6 py-4 rounded-t-lg ${
+                activeTab === "signup" && "text-white bg-black"
+              }`}
+            >
+              新規登録
+            </button>
+          </li>
+          <li className="me-2">
+            <button
+              onClick={() => setActiveTab("login")}
+              className={`inline-block px-6 py-4 rounded-t-lg ${
+                activeTab === "login" && "text-white bg-black"
+              }`}
+            >
+              ログイン
+            </button>
+          </li>
+        </ul>
+      </div>
+      <div className="flex justify-center pt-6">
+        {activeTab === "signup" ? <h2>新規登録</h2> : <p>ログインフォーム</p>}
+      </div>
     </div>
   );
 }
