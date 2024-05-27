913930a1883745804acf6db00dbda01e729710e0
yukimatsu <drying3@gmail.com>
2024-05-27 16:18:45 +0900

    ♻️ chakra UIを使ってAuthFormをリファクタリング

--- src/app/components/AuthForm.tsx
+++ src/app/components/AuthForm.tsx
@@ -1,39 +1,56 @@
 "use client";
+import {
+  Tabs,
+  TabList,
+  TabPanels,
+  Tab,
+  TabPanel,
+  Stack,
+  Button,
+  Box,
+} from "@chakra-ui/react";
+import { Input } from "@chakra-ui/react";
 
-import { useState } from "react";
-
-// ToDo: 5/26 フォームUI作成とusername,email,passwordを登録するようにする。画像はデフォルトを設定し、後からアップロードさせる（モーダルで表示もありだと思う）
 export default function AuthForm() {
-  const [activeTab, setActiveTab] = useState("signup");
   return (
-    <div className="flex flex-col">
-      <div>
-        <ul className="flex flex-wrap text-sm font-medium text-center text-gray-500 border-b border-gray-200">
-          <li className="me-2">
-            <button
-              onClick={() => setActiveTab("signup")}
-              className={`inline-block px-6 py-4 rounded-t-lg ${
-                activeTab === "signup" && "text-white bg-black"
-              }`}
+    <>
+      <Box
+        px={12}
+        py={8}
+        border="1px"
+        borderRadius="md"
+        borderColor="gray.200"
+        boxShadow="sm"
+      >
+        <Tabs isFitted variant="unstyled" width="100%" maxWidth="400px">
+          <TabList>
+            <Tab
+              _selected={{ color: "white", bg: "black", borderTopRadius: "4" }}
             >
               新規登録
-            </button>
-          </li>
-          <li className="me-2">
-            <button
-              onClick={() => setActiveTab("login")}
-              className={`inline-block px-6 py-4 rounded-t-lg ${
-                activeTab === "login" && "text-white bg-black"
-              }`}
-            >
-              ログイン
-            </button>
-          </li>
-        </ul>
-      </div>
-      <div className="flex justify-center pt-6">
-        {activeTab === "signup" ? <h2>新規登録</h2> : <p>ログインフォーム</p>}
-      </div>
-    </div>
+            </Tab>
+            <Tab _selected={{ color: "white", bg: "black" }}>ログイン</Tab>
+          </TabList>
+
+          <TabPanels>
+            <TabPanel minHeight="250px">
+              <Stack spacing={3}>
+                <Input placeholder="ユーザーネーム" />
+                <Input placeholder="メールアドレス" />
+                <Input placeholder="パスワード" />
+                <Button colorScheme="blue">新規登録</Button>
+              </Stack>
+            </TabPanel>
+            <TabPanel minHeight="250px">
+              <Stack spacing={3}>
+                <Input placeholder="ユーザーネーム" />
+                <Input placeholder="メールアドレス" />
+                <Button colorScheme="blue">ログイン</Button>
+              </Stack>
+            </TabPanel>
+          </TabPanels>
+        </Tabs>
+      </Box>
+    </>
   );
 }
