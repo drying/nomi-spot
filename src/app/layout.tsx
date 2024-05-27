42222e9086ee9904fd4178a773a9182f7184bed5
yukimatsu <drying3@gmail.com>
2024-05-27 16:18:07 +0900

    ♻️ フォントの設定

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
   return (
     <html lang="ja">
       {/* <body className={inter.className}>{children}</body> */}
-      <body>
+      <body className={inter.className}>
         <Providers>{children}</Providers>
       </body>
     </html>
